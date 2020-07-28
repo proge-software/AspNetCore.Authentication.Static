cf6d0e0f009aa37ff133fc4c04be4a0addb3d6b8
Giorgio Di Nardo <github@akelitz.net>
2020-07-28 23:24:36 +0200

    Add ASP.NET Core MVC sample (#6)
    Co-authored-by: Giorgio Di Nardo <akelitz@hotmail.com>

---
+++ samples/MvcSample/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcSample.Models;
+using System.Diagnostics;
+
+namespace MvcSample.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = Permissions.IsAdmin)]
+        public IActionResult AdminOnly()
+        {
+            return View(nameof(Index));
+        }
+
+        [Authorize(Policy = Permissions.IsUser)]
+        public IActionResult UserOnly()
+        {
+            return View(nameof(Index));
+        }
+
+        [Authorize(Policy = Permissions.IsAdminOrUser)]
+        public IActionResult AdminOrUser()
+        {
+            return View(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ samples/MvcSample/Permissions.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcSample
+{
+    public static class Permissions
+    {
+        public const string IsAdmin = nameof(IsAdmin);
+
+        public const string IsUser = nameof(IsUser);
+
+        public const string IsAdminOrUser = nameof(IsAdminOrUser);
+    }
+}
